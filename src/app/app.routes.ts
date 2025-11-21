bc026645c67283c6051b9f7d239b729c71ca40b1
devilbd <vladymir.parchev@gmail.com>
2025-11-22 00:03:59 +0200

    ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: 'typing',
+        path: '',
         loadComponent: () => import('./typing/typing.component').then(m => m.TypingComponent)
     }
 ];
